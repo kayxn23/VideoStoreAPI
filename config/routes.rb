f4ca883b571353e4a6e0622b06c7dd0ce3634025
Kay <kayxn23@gmail.com>
2018-11-05 12:39:44 -0800

    pseudocoded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read the API Requirements below and create a pseudo-code "routes" file that specifies
+  # The endpoints your API will need
+  # The HTTP verbs each endpoint will use
+  # Any data that must be provided to the endpoint in order for it to do its work
+
+  #get customers -> returns id, name registered_at, postal_code, phone
+  #get movies
+  #get movies/:id
+  #post movies  -> returns id of the new movie just created
+  #post /rentals/check-out
+  #post /rentals/check-in
+
 end
