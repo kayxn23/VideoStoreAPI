1f75aaf1679a3f12c86d77e32b3937e100bd097a
Kay <kayxn23@gmail.com>
2018-11-05 12:46:07 -0800

    added sample route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Read the API Requirements below and create a pseudo-code "routes" file that specifies
-  # The endpoints your API will need
-  # The HTTP verbs each endpoint will use
-  # Any data that must be provided to the endpoint in order for it to do its work
+  get '/customers', to: 'customers#index' 
 
   #get customers -> returns id, name registered_at, postal_code, phone
   #get movies
