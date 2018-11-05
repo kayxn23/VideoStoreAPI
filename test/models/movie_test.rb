242a4c138e2d5e5e44559a4f3001d872d38a0fec
Jazz <jazziesf21@yahoo.com>
2018-11-05 15:53:17 -0800

    movie model testing

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,6 +1,5 @@
 require "test_helper"
 
-<<<<<<< HEAD
 
 # has_many :rentals
 # has_many :customers, through: :rentals
@@ -52,36 +51,14 @@ describe Movie do
       rentals.each do |rental|
         expect(rental).must_be_instance_of Rental
       end
+    end
 
+    it 'can has a customer through rentals' do
+      rental = rentals(:rental2)
+      expect(rental.customer).must_be_instance_of Customer
+      expect(rental.customer).must_respond_to :name
     end
   end
 
 
 end
-=======
-# describe Movie do
-#   let(:movie) { movies(:movie1) }
-#
-#   it "must be valid" do
-#     value(movie).must_be :valid?
-#   end
-#
-#   describe 'Relationships' do
-#     # has_many :rentals
-#     # has_many :customers, through: :rentals
-#     it 'can have many rentals' do
-#       movie.customers << rentals(:rental1)
-#       movie.customers << rentals(:rental2)
-#       rentals = movie.rentals
-#
-#       expect(rentals.length).must_be :>=, 2
-#       rentals.each do |rental|
-#         expect(rental).must_be_instance_of Rental
-#       end
-#
-#     end
-#   end
-#
-#
-# end
->>>>>>> 9c1a44fe0f96620141c4bda99568e6d245a3fb26
