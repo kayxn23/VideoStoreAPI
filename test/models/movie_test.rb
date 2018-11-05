9c1a44fe0f96620141c4bda99568e6d245a3fb26
Kay <kayxn23@gmail.com>
2018-11-05 15:31:24 -0800

    added have many movies thru rentals relationship test to customer

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 # describe Movie do
-#   let(:movie) { Movie.new }
+#   let(:movie) { movies(:movie1) }
 #
 #   it "must be valid" do
 #     value(movie).must_be :valid?
 #   end
+#
+#   describe 'Relationships' do
+#     # has_many :rentals
+#     # has_many :customers, through: :rentals
+#     it 'can have many rentals' do
+#       movie.customers << rentals(:rental1)
+#       movie.customers << rentals(:rental2)
+#       rentals = movie.rentals
+#
+#       expect(rentals.length).must_be :>=, 2
+#       rentals.each do |rental|
+#         expect(rental).must_be_instance_of Rental
+#       end
+#
+#     end
+#   end
+#
+#
 # end
