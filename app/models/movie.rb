d33b7e07df496012169d0cdf05c00b2d8d58e8c9
Katie Jahanmir <katietj@gmail.com>
2018-11-05 16:56:54 -0800

    added movie model validations and

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
-  # validates :id, presence: true
   validates :title, presence: true
   validates :release_date, presence: true
+  validates :overview, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
