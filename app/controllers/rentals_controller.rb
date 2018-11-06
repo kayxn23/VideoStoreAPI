bb64be5ab05caa6f9ad67e0147b7acee816639a6
Kay <kayxn23@gmail.com>
2018-11-06 15:12:58 -0800

    created rental controller with checkout and checkin methods

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,34 @@
+class RentalsController < ApplicationController
+  def checkout
+
+    rental = Rental.new(rental_params)
+    customer = Customer.find_by(id: params[:id])
+    movie = Movie.find_by(id: params[:id])
+    rental.customer_id = customer.id
+    rental.movie_id = movie.id
+    
+    rental.checkout_date = Date.today
+    rental.due_date = Date.today + 7
+
+    if rental.save
+      render json: rental.as_json(only: [:customer_id, :movie_id]),
+      status: :ok
+    else
+      render json: {
+        ok: false,
+        message: rental.errors.messages
+        }, status: :bad_request
+      end
+    end
+
+    def checkin
+    end
+
+
+  private
+
+  def rental_params
+    params.permit(:customer_id, :movie_id, :checkout_date, :due_date )
+  end
+
+end
---
+++ test/controllers/rentals_controller_test.rb
@@ -0,0 +1,68 @@
+require "test_helper"
+
+describe RentalsController do
+
+  # it "should get checkout" do
+  #   post checkout_path
+  #   value(response).must_be :success?
+  # end
+
+  # it "should get checkin" do
+  #   post checkin_path
+  #   value(response).must_be :success?
+  # end
+
+
+
+
+
+  describe "create (checkout)" do
+    let(:rental_data) {
+      {
+        customer_id: 1,
+        movie_id: 2,
+        checkout_date: Date.today,
+        due_date: 10
+      }
+    }
+
+    it "creates a new rental given valid data" do
+      expect {
+      post checkout_path, params: { rental: rental_data }
+    }.must_change "Rental.count", 1
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body["rental"]).must_include "id"
+
+      rental = Rental.find(body["rental"]["id"].to_i)
+
+      expect(rental.customer_id).must_equal rental_data[:customer_id]
+      must_respond_with :success
+
+    end
+
+    # it "returns an error for invalid movie data" do
+    #   # arrange
+    #   movie_data["title"] = nil
+    #
+    #   expect {
+    #   post movies_path, params: { movie: movie_data }
+    # }.wont_change "Movie.count"
+    #
+    #   body = JSON.parse(response.body)
+    #
+    #   expect(body).must_be_kind_of Hash
+    #   expect(body).must_include "message"
+    #   expect(body["message"]).must_include "title"
+    #   must_respond_with :bad_request
+    # end
+
+  end
+
+
+
+
+
+end
