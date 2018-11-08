987608d8f91b91e9ab86ecb15f2dcce6d4647c61
Jazz <jazziesf21@yahoo.com>
2018-11-08 12:02:32 -0800

    added query to project

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,21 +1,25 @@
 class CustomersController < ApplicationController
+
+  SORTABLE_ATTRIBUTES = [:name, :registered_at, :postal_code]
+
   def index
     customers = Customer.all
 
-    if !customer_params["sort"].nil?
-      customers = Customer.order(customer_params["sort"])
-      render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
-      status: :ok
-    else
-      render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
-      status: :ok
+    if params[:sort].present? && SORTABLE_ATTRIBUTES.include?(params[:sort].to_sym)
+      customers = customers.order(params[:sort].to_sym => :asc)
     end
+
+    if params[:n].present?
+      customers = customers.limit(params[:n].to_i)
+    end
+
+    if params[:p].present?
+      customers = customers.offset((params[:p].to_i - 1) * (params[:n].to_i))
+    end
+
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
+    status: :ok
+
   end
-
-  private
-
-  def customer_params
-    params.permit(:sort, :n, :p)
-  end
-
+  
 end
